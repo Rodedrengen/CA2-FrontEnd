63484c95b15baff9519bd83e585c8c00183f0e40
Simon Jørgensen <simon.588.jorgensen@gmail.com>
2020-10-19 16:05:49 +0200

    first

---
+++ src/navigation.js
@@ -0,0 +1,69 @@
+import { getAll, getByID, addUser, editUser, deleteUser } from './index';
+// Navigation
+// To add more top level buttons in comment the out commented code below and in public/index.html
+const btnEx1 = document.getElementById('btnEx1');
+const btnEx2 = document.getElementById('btnEx2');
+const btnEx3 = document.getElementById('btnEx3');
+const btnEx4 = document.getElementById('btnEx4');
+const btnEx5 = document.getElementById('btnEx5');
+const sectionEx1 = document.getElementById('1');
+const sectionEx2 = document.getElementById('2');
+const sectionEx3 = document.getElementById('3');
+const sectionEx4 = document.getElementById('4');
+const sectionEx5 = document.getElementById('5');
+const s1Content = sectionEx1.innerHTML;
+const s2Content = sectionEx2.innerHTML;
+const s3Content = sectionEx3.innerHTML;
+const s4Content = sectionEx4.innerHTML;
+const s5Content = sectionEx5.innerHTML;
+
+removeContent();
+
+btnEx1.onclick = (e) => {
+  e.preventDefault();
+  removeContent();
+  insertContent(1);
+  getAll();
+};
+
+btnEx2.onclick = (e) => {
+  e.preventDefault();
+  removeContent();
+  insertContent(2);
+  getByID();
+};
+
+btnEx3.onclick = (e) => {
+  e.preventDefault();
+  removeContent();
+  insertContent(3);
+  addUser();
+};
+btnEx4.onclick = (e) => {
+  e.preventDefault();
+  removeContent();
+  insertContent(4);
+  editUser();
+};
+btnEx5.onclick = (e) => {
+  e.preventDefault();
+  removeContent();
+  insertContent(5);
+  deleteUser();
+};
+
+function removeContent() {
+  sectionEx1.innerHTML = '';
+  sectionEx2.innerHTML = '';
+  sectionEx3.innerHTML = '';
+  sectionEx4.innerHTML = '';
+  sectionEx5.innerHTML = '';
+}
+
+function insertContent(id) {
+  if (id === 1) sectionEx1.innerHTML = s1Content;
+  if (id === 2) sectionEx2.innerHTML = s2Content;
+  if (id === 3) sectionEx3.innerHTML = s3Content;
+  if (id === 4) sectionEx4.innerHTML = s4Content;
+  if (id === 5) sectionEx4.innerHTML = s5Content;
+}
